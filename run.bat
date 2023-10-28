433197962d1bf777c91a39a682c00aa3fd908c44
D-Mad <thanh.datit1992@gmail.com>
2023-10-28 13:12:34 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,7 @@
 @echo off
 
-
 call .\venv\Scripts\activate.bat
 
 set CUDA_VISIBLE_DEVICES=0
-python inference_pgdiff.py --task restoration --in_dir testdata/cropped_faces --out_dir results/blind_restoration --guidance_scale 0.05
-pause
+
+cmd /k
